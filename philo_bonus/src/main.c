c7deba8dfac9be8b5d9002f7d03927bb7e8fdfbe
Your Name <you@example.com>
2022-06-15 07:25:07 +0300

    doesnt work on wsl

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -12,50 +12,6 @@
 
 #include "philosophers.h"
 
-void	check_death(t_args *args)
-{
-	long long	curr;
-	long long	death_time;
-
-	death_time = args->last_meal + args->c.time_to_die;
-	curr = get_current_time_us();
-	if (death_time <= curr)
-	{
-		print_action(args->print, args->id, DEAD, 0);
-		pthread_exit(0);
-	}
-	else if (death_time <= curr + args->c.time_to_eat)
-	{
-		usleep_ms(curr + args->c.time_to_eat - death_time);
-		print_action(args->print, args->id, DEAD, 0);
-		pthread_exit(0);
-	}
-}
-
-void	check_death_with_forks(t_args *args)
-{
-	long long	curr;
-	long long	death_time;
-
-	death_time = args->last_meal + args->c.time_to_die;
-	curr = get_current_time_us();
-	if (death_time <= curr)
-	{
-		sem_post(args->forks);
-		sem_post(args->forks);
-		print_action(args->print, args->id, DEAD, 0);
-		pthread_exit(0);
-	}
-	else if (death_time <= curr + args->c.time_to_eat)
-	{
-		sem_post(args->forks);
-		sem_post(args->forks);
-		usleep_ms(curr + args->c.time_to_eat - death_time);
-		print_action(args->print, args->id, DEAD, 0);
-		pthread_exit(0);
-	}
-}
-
 void	*philosopher(void *arg)
 {
 	t_args	*args;
@@ -104,15 +60,34 @@ void	init_args_n_arr(t_args *args)
 	args->last_meal = get_current_time_us();
 }
 
-void	create_sem(char *name)
+void	create_sem(char *name, int value)
 {
 	t_sem	*new;
 
 	sem_unlink(name);
 	new = (t_sem *)ft_memalloc(sizeof(t_sem));
 	new->name = ft_strdup(name);
-	new->sem = sem_open(new->name, O_CREAT, S_IRWXU, 1);
+	new->sem = sem_open(new->name, O_CREAT, S_IRWXU, value);
 	return new;
+}
+
+void	init_philosophers(t_data *data, t_data *data_arr)
+{
+	int		i;
+	t_phi	*curr_p;
+	
+	i = 0;
+	while (++i < data->c->p_num + 1)
+	{
+		curr_p = &data->phi[i];
+		curr_p->id = i;
+		curr_p->left_fork = get_fork_id(i, data->c->p_num, 1);
+		curr_p->right_fork = get_fork_id(i, data->c->p_num, 0);
+		curr_p->status = THINK;
+		curr_p->must_eat_times = data->c->must_eat_times;
+		curr_p->last_meal = get_current_time_ms();
+		data_arr[i].my_id = i;
+	}
 }
 
 void	init_data(t_data *data)
@@ -125,14 +100,15 @@ void	init_data(t_data *data)
 			(data->c->p_num + 1));
 	data->process_mon = (pid_t *)ft_memalloc(sizeof(pid_t));
 	
-	data->done_sem = create_sem("done");
-	data->print_sem = create_sem("print");
-	data->dead_sem = create_sem("dead");
-	data->meals_sem = create_sem("meals counter");
+	data->done_sem = create_sem("done", 1);
+	data->print_sem = create_sem("print", 1);
+	data->dead_sem = create_sem("dead", 1);
+	data->meals_sem = create_sem("meals counter", 1);
+	data->forks_common = create_sem("forks common", data->c->p_num);
 	i = 0;
 	while (++i < data->c->p_num + 1) {
 		asprintf(&sem_name, "fork_%d", i);
-		data->forks_sem[i] = create_sem(sem_name);
+		data->forks_sem[i] = create_sem(sem_name, 1);
 		free(sem_name);
 	}
 }
@@ -147,8 +123,7 @@ int	main(int ac, char **av)
 	if (i && is_const_valid(data.c, ac, av)) {
 		init_data(&data);
 		init_monitor(&data);
-		data_arr = ft_memalloc(sizeof(t_data) * (data.c->p_num + 1));
-		init_philosophers(&data, data_arr);
+		init_philosophers(&data);
 
 		while (++i < args.num + 1)
 		{
